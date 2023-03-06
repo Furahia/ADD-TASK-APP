314bbc41e1cc47217c02f6d73c7ea470946d6419
Furahia <brendahjoy5@gmail.com>
2023-03-06 09:32:03 +0300

    add files

---
+++ src/components/users.js
@@ -0,0 +1,14 @@
+export const admins = [
+    {
+        username: "brendah",
+        password: "123"
+    },
+    {
+        username: "bones",
+        password: "abc"
+    },
+    {
+        username: "njambi",
+        password: "xyz"
+    }
+]
